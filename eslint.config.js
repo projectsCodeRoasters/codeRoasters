33cfa9d61150157b548f8201cb6eba70ed5c4030
Guillermo Mozo <guillermomozolopez@gmail.com>
2025-08-29 20:58:15 +0200

    inital commit

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { ignores: ['dist'] },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    settings: { react: { version: 'detect' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...(react.configs['jsx-runtime']?.rules ?? {}),
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-multi-spaces': 'error', // 🔴 obliga a no usar espacios de más
+      'no-trailing-spaces': 'error', // 🔴 sin espacios al final de línea
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+      "semi": ["error", "always"],
+    },
+  },
+];
---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// prettier.config.js
+export default {
+  semi: true, // Punto y coma al final
+  singleQuote: true, // Comillas simples
+  trailingComma: 'all', // Comas finales en todo (mejor para diffs)
+  printWidth: 100, // Un poco más ancho que 80
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxSingleQuote: false, // En JSX comillas dobles
+  endOfLine: 'lf', // Consistente entre sistemas
+};
