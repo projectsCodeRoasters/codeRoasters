e969efc00744e303661de24d8df5847ee2829f01
Guillermo Mozo <guillermomozolopez@gmail.com>
2025-10-11 13:58:31 +0200

    Initial commit

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -19,7 +19,7 @@ function Dashboard() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>codeRoaster</h1>
+      <h1>Code Roasters</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
