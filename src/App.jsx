ae21d203771e63f3bb09434d7c0df20ff36904fc
gmozo <guillermomozolopez@gmail.com>
2025-10-23 11:13:27 +0200

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
-import { AuthProvider } from "./contexts/AuthContext";
-import Logged from "./pages/Loged";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import appLogo from "../public/favicon.svg";
+import PWABadge from "./PWABadge";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/logado" element={<Logged />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
-    </Router>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={appLogo} className="logo" alt="codeRoaster logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Code Roasters</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <PWABadge />
+    </>
   );
 }
 
--- src/hooks/useAuth.js
+++
@@ -1,5 +0,0 @@
-// src/auth/useAuth.js
-import { useContext } from "react";
-import AuthContext from "../contexts/AuthContext";
-
-export const useAuth = () => useContext(AuthContext);
